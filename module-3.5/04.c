6a1e74f944fe54367fa7b9686c08acf1d54f36c5
rokib97 <hasanrokib1997@gmail.com>
2024-05-10 18:38:43 +0600

    module 3.5 completed

---
+++ module-3.5/04.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a;
+    long long int b;
+    float c;
+    char d;
+    scanf("%d", &a);
+    scanf("%lld", &b);
+    scanf("%f", &c);
+    scanf(" %c", &d);
+    printf("%d\n", a);
+    printf("%lld\n", b);
+    printf("%0.2f\n", c);
+    printf("%c\n", d);
+ 
+
+    return 0;
+}
