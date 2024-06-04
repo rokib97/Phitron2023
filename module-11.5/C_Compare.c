05c8fa9dfcdcca8820f5525b2caa7eca1d4daf72
rokib97 <hasanrokib1997@gmail.com>
2024-06-05 01:08:02 +0600

    problem 1 solved

---
+++ module-11.5/C_Compare.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    char a[21],b[21];
+    scanf("%s %s",a,b);
+    int value = strcmp(a,b);
+    if(value < 0){
+        printf("%s\n",a);
+    }else if(value>0){
+        printf("%s\n",a);
+    }else{
+         printf("%s\n",a);
+    }
+    return 0;
+}
